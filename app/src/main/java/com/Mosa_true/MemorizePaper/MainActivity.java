1930532549376f2f0ac844c3caf7187e0d9fc554
harakoo321 <kouseih321@gmail.com>
2023-06-17 13:30:04 +0900

    プロジェクトの作成 ・MainActivityの作成 ・Zstd、OpenCVの導入

---
+++ app/src/main/java/com/Mosa_true/MemorizePaper/MainActivity.java
@@ -0,0 +1,60 @@
+package com.Mosa_true.MemorizePaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == LoaderCallbackInterface.SUCCESS) {
+                Log.i("OpenCV", "OpenCV loaded successfully");
+            } else {
+                super.onManagerConnected(status);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button readButton = findViewById(R.id.readButton);
+        Button writeButton = findViewById(R.id.writeButton);
+        ButtonClickListener buttonClickListener = new ButtonClickListener();
+        readButton.setOnClickListener(buttonClickListener);
+        writeButton.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (!OpenCVLoader.initDebug()){
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    private class ButtonClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            int id = view.getId();
+            if (id == R.id.readButton){
+                
+            } else if (id == R.id.writeButton) {
+                
+            }
+        }
+    }
+}
